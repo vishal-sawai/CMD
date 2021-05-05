d6c2441fe107a91e8f14c95db714670f39eb285d
vishal sawai <vishalsawai1725@gmail.com>
2021-05-05 15:53:10 +0530

    some

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <div>
+        <nav>
+        <ul>
+            <li class=""><a href="#home">Home</a></li>
+            <li class=""><a href="#project">Course</a></li>
+            <li class=""><a href="#about">About</a></li>
+            <li class=""><a href="#contact">Contact</a></li>
+        </ul>
+        </nav>
+    </div>
 </body>
+
 </html>
